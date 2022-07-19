64b57d2cdbae24ff636947b67a3e82ad89fc5d3e
russ960 <russ@johnsonville.net>
2022-07-19 07:57:43 -0500

    Completion of Day 61

--- 61/main.py
+++ 61/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, validators
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 app.secret_key = "iIS3gE33R6U4Rt52"
 
 class LoginForm(FlaskForm):
@@ -19,7 +21,10 @@ def login():
     form = LoginForm(request.form)
     print(form.validate_on_submit())
     if request.method == 'POST' and form.validate_on_submit():
-        return render_template('success.html')
+        if form.email.data == 'admin@email.com' and form.password.data == '12345678':
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
     return render_template('login.html', form=form)
 
 
